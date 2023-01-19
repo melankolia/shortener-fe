8621cf9b008d308b6de9c6b98a2236ce24909c07
fahron <fahrononasutra@gmail.com>
2023-01-20 05:21:14 +0700

    page not found and page expired

--- src/views/ExpiredView.vue
+++ src/views/ExpiredView.vue
@@ -1,15 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="expired flex flex-col h-screen ">
+    <div
+      class="flex flex-col h-screen justify-center items-center text-center "
+    >
+      <p class="header-1 leading-normal  text-white pt-40 pb-5">Sorry! your short <br/>link expired</p>
+      <p class="body-1 text-white text-center pb-10">
+        Your short link has expired, shorten your link again.
+      </p>
+      <button
+          class="rounded-lg  h-16 w-52  "
+          style="background-color: #3ed078"
+        >
+          <span class="caption-1 text-white font-semibold"> Shorten again </span>
+        </button>
+    </div>
+   
+    <div class="flex flex-col items-center justify-end h-1/2 mb-3">
+      <p class="subtitle-1 text-center text-white">
+        Copyright © 2023 cawaproject. <br class="block md:hidden" />
+        All Right Reserved.
+      </p>
+    </div>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
+
+  .expired {
+    background-image: url("@/assets/background-full.svg");
+    background-position: center;
+
   }
-}
 </style>
--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,15 +1,36 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+ <div class="notfound flex flex-col h-screen relative">
+    <div
+      class="flex flex-col h-screen justify-center items-center text-center "
+    >
+      <p class="text-9xl font-semibold text-white pt-40">404</p>
+      <p class="body-1 text-white text-center pb-10">
+        Page not found.
+      </p>
+      <button
+          class="rounded-lg  h-16 w-52  "
+          style="background-color: #3ed078"
+        >
+          <span class="caption-1 text-white font-semibold"> Back to shortener </span>
+        </button>
+    </div>
+   
+    <div class="flex flex-col items-center justify-end h-1/2 mb-3">
+      <p class="subtitle-1 text-center text-white">
+        Copyright © 2023 cawaproject. <br class="block md:hidden" />
+        All Right Reserved.
+      </p>
+    </div>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
+
+  .notfound {
     min-height: 100vh;
     display: flex;
     align-items: center;
+    background-image: url("@/assets/background-full.svg");
+    background-position: center;
   }
-}
 </style>
